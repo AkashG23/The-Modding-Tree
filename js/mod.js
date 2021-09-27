ce9bca3b54527282b3818687c99cc5e29a36794c
AkashG23 <91497555+AkashG23@users.noreply.github.com>
2021-09-27 15:23:08 -0400

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
 	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
+	id: "sussy-moogus",
+	author: "akashg23",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
 
